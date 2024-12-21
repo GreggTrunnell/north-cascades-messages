ed2cdcccff8732b2f9708ae3b4899d03908ab4bd
Gregg Trunnell <gggtrunnell@gmail.com>
2024-12-21 15:57:55 -0600

    created disabled code for button

--- src/components/NewMessage/NewMessage.jsx
+++ src/components/NewMessage/NewMessage.jsx
@@ -18,11 +18,13 @@ function NewMessage ( {fetchMessages} ) {
   return (
     <div>
       <h1>New Message</h1>
-      <form>
       <TextField  type='Name' placeholder='NAME' onChange={ (e)=>{ setCurrentMessage( {...currentMessage, name: e.target.value } ) } } />       
       <TextField  type='text' placeholder='MESSAGE' onChange={ (e)=>{ setCurrentMessage( {...currentMessage, text: e.target.value } ) } } /> 
-      <Button onClick={ sendMessage }>Send</Button>
-      </form>
+       {
+       currentMessage.name === '' || currentMessage.text === '' ? 
+      <Button disabled variant="contained" onClick={ sendMessage }>Send</Button>:
+      <Button variant="contained" onClick={ sendMessage }>Send</Button>
+      }
     </div>
   );
 }
