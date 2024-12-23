50a4727eba734b1f1d1ee298083583401be9efef
Gregg Trunnell <gggtrunnell@gmail.com>
2024-12-22 22:12:41 -0600

    working getting a message to go to database

--- server/routes/messages.router.js
+++ server/routes/messages.router.js
@@ -4,7 +4,11 @@ const pool = require('../modules/pool.js')
 
 let messages = [];
 
-router.get( '/', ( _req, res )=>{
+// router.get( '/', ( req, res )=>{
+//     res.send( messages );
+// })
+
+router.get( '/', ( req, res )=>{
     const queryText = `SELECT * FROM "messages" ORDER BY id ASC;`;
     pool.query( queryText )
     .then(( results )=>{
@@ -14,7 +18,6 @@ router.get( '/', ( _req, res )=>{
         console.log('error in message.router get', err);
         res.sendStatus( 400 );
     })
- 
 })
 
 router.post( '/', ( req, res )=>{
